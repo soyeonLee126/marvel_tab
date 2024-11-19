03e964c010d3b0bbc18d518fd9c9fdc17034409b
torder-raven <109227292+soyeonLee126@users.noreply.github.com>
2024-11-19 11:41:01 +0900

    usecase 모듈 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     alias(libs.plugins.google.ksp) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.google.dagger.hilt.android) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
 }
---
+++ core/usecase/src/main/java/com/example/marvel_tab/core/usecase/GetFavoriteCharactersUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.marvel_tab.core.usecase
+
+import javax.inject.Inject
+
+class GetFavoriteCharactersUseCase @Inject constructor() {
+    operator fun invoke() {
+
+    }
+}
